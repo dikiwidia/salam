689092d927ab218f2f7353f31b6c75b6a75d9152
Moch Diki Widianto <dikiwidia@live.com>
2019-04-21 23:38:53 +0700

    Fixed Bugs & Update README.md

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \App\Http\Middleware\HttpsProtocol::class,
+        //\App\Http\Middleware\HttpsProtocol::class,
         //\Illuminate\View\Middleware\ShareErrorsFromSession::class,
     ];
 
